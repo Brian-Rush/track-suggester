115e67379a0cbd5b6305c456b6148de4c98dbe8f
Brian Rush <yankeh@gmail.com>
2017-04-28 10:06:50 -0700

    add html header and content (first two questions with radio buttons)

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+//Are you more interested in the way things work 'under the hood' or are you more interested in the way things look and the way people interact with things?
+
+//if 'under the hood'
+//What kind of environment would you like to work in?
+//a structured environment with good benefits and clearly defined protocols
+//a casual but fast-paced environment with lots of variety, plenty of creative freedom, and potentially long hours
+//a very fast-paced environment with lots of excitement, uncertainty, and learning
+
+
+
+//if looks and interaction
+//in the previous question which is more interesting to you:
+//how people interact with things or how things look?
