d670c253f1ea31382bcae2669b46ed68436e69ee
Brian Rush <yankeh@gmail.com>
2017-04-28 12:41:43 -0700

    add all foreseeable html content

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,36 @@
-//Are you more interested in the way things work 'under the hood' or are you more interested in the way things look and the way people interact with things?
+//0. Would you like to study full-time or part-time?
+//user submits
+//if full time, display Q1.
+//if part time, display info about part time course and how they can join up with other track later.
 
-//if 'under the hood'
-//What kind of environment would you like to work in?
+//1.Are you more interested in the way things work 'under the hood' or are you more interested in the way things look and the way people interact with things?
+
+// User pushes button. UI shows next Q based on their input.
+
+//IF 'UNDER THE HOOD' ask two Qs
+//2A. What kind of environment would you like to work in?
 //a structured environment with good benefits and clearly defined protocols
 //a casual but fast-paced environment with lots of variety, plenty of creative freedom, and potentially long hours
 //a very fast-paced environment with lots of excitement, uncertainty, and learning
 
+//3ai. if structured display Java/Android
+//3aii. if casual display Ruby, Java, (note about agencies serving enterprise = wide range of languages used)
+//3aiii. if
+//3aiv. if
+
+//2aa. Which of these platforms would you most like to work with? Microsoft / PC, Android, Apple, No preference/Don't Know
+
+//3aai. if Microsoft, show C#/.net info,
+//3aai. if Android, show Java/Android info,
+//3aai. if Apple, show Ruby/Rails
+//3aai. if no pref, show Ruby/Rails, with links to others
 
 
-//if looks and interaction
-//in the previous question which is more interesting to you:
-//how people interact with things or how things look?
+
+
+//2b. IF LOOK AND INTERACTION
+//in the previous question which is more interesting to you: how people interact with things or how things look?
+
+//if interaction, display info paragraph about React track
+
+//if looks, display info paragraph about css/design track
