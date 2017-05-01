8c06a69e7ee776a2b1f736f27f8746b048567ff7
Brian Rush <yankeh@gmail.com>
2017-04-30 23:10:49 -0700

    get the scripts starting to work, but still need to fix the double-submit issue.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,29 @@
 //Business logic
 
+// var answer0 = $("form#question-zero").submit();
+
 // User Interface Logic
 $(document).ready(function() {
   $("form").submit(function(event) {
-    event.preventDefault();
-  });
+    var answerZero = $("input[type=radio][name=radio-group-zero]:checked").val();
+    // if answerZero =
 
-  $(function() {
-    $("#part-time-info").show();
+    $('#question-zero').submit(function() {
+      if (answerZero = "part-time") {
+        $('#part-time-info').show();
+      } else {
+        $('#back-or-front-q-div').show();
+      }
+    })
+    //   $("input[type=radio][name=radio-group-zero]:checked").val();
+    // });
+    event.preventDefault()
   });
 });
+
+
+
+//   $(function() {
+//     $("#part-time-info").show();
+//   });
+// });
