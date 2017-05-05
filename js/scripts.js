dda736a37795f0e6e1f397526b26f3d4f7ff3170
Brian-Rush <yankeh@gmail.com>
2017-05-05 02:03:14 -0700

    add index2.html file used to clean up HTML from original index file. It will also make refactoring modifications to the js and CSS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,30 +1,48 @@
 //Business logic
 
-// var answer0 = $("form#question-zero").submit();
+
+
 
 // User Interface Logic
+
 $(document).ready(function() {
   $("form").submit(function(event) {
-    var answerZero = $("input[type=radio][name=radio-group-zero]:checked").val();
-    var answerOne = $("input[type=radio][name=radio-group-zero]:checked").val();
+    event.preventDefault();
 
-    // $('#question-zero').submit(function() {
-      if (answerZero === "part-time") {
-        $('#part-time-info').show();
-      } else {
-        $('#back-or-front-q-div').show();
-        $('#question-zero').hide();
-      }
-    // })
-    //   $("input[type=radio][name=radio-group-zero]:checked").val();
-    // });
-    event.preventDefault()
+    var answerZero = $("input[type=radio][name=radio-group-zero]:checked").val();
+    var answerOne = $("input[type=radio][name=radio-group-one]:checked").val();
+    var answerTwoFront = $("input[type=radio][name=radio-group-two-front]:checked").val();
+    var answerTwoBack = $("input[type=radio][name=radio-group-two-back]:checked").val();
+    var answerThree = $("input[type=radio][name=radio-group-three]:checked").val();
+
+
+    // Question Zero
+    if (answerZero === "part-time") {
+      $('.initially-hidden').hide();
+      // $('#question-zero').hide();
+      $('#part-time-info').show();
+    } else if (answerZero === "full-time") {
+      $('.initially-hidden').hide();
+      $('#question-zero').hide();
+      $('#question-one').show();
+    }
+
+    // Question One
+    if (answerOne === "look-interact") {
+      $('.initially-hidden').hide();
+      $('#question-two-front').show();
+    } else if (answerOne === "under-hood"){
+      $('.initially-hidden').hide();
+      $('#question-two-back').show();
+    }
+
+    // Question Two-Front
+    if (answerTwoFront === "look") {
+      $('.initially-hidden').hide();
+      $('#css-design-info').show();
+    } else if (answerTwoFront === "interact") {
+      $('.initially-hidden').hide();
+      $('#php-react-info').show();
+    }
   });
 });
-
-
-
-//   $(function() {
-//     $("#part-time-info").show();
-//   });
-// });
